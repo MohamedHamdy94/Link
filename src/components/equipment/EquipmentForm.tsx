1108a8a28cb924c68ac4dba3a5a33decf888bac7
Mohamed Hamdy Ahmed Mohamed <mohamedhamdyahmed94@gmail.com>
2025-06-02 19:11:18 +0300

    Updated the Project

--- src/components/equipment/EquipmentForm.tsx
+++ src/components/equipment/EquipmentForm.tsx
@@ -355,7 +355,7 @@ const EquipmentForm = () => {
       );
       
       if (uploadResult && uploadResult.success) {
-        photoUrl as string = uploadResult.url; // تعيين قيمة photoUrl إذا كان التحميل ناجحاً
+        photoUrl as string = uploadResult.url || 'فشل في رفع صورة المعدة'
 
       }else{
         photoUrl=''
