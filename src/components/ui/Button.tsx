3b96b2ca1b7fc48c6a20945648908f18254dd00b
Mohamed Hamdy Ahmed Mohamed <mohamedhamdyahmed94@gmail.com>
2025-07-14 18:31:19 +0300

    Updated the Project error

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,10 +1,6 @@
 import React from 'react';
 
-interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
-  // You can add custom props here if needed
-}
-
-const Button: React.FC<ButtonProps> = ({ className = '', children, ...props }) => {
+const Button: React.FC<React.ButtonHTMLAttributes<HTMLButtonElement>> = ({ className = '', children, ...props }) => {
   return (
     <button
       className={`inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 ${className}`}
